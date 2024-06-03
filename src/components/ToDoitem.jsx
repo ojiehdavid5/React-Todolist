4d52382c83b4131dca4b6e66785c2ddd45ca1fad
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-06-03 16:19:22 +0100

    changes

--- src/components/ToDoitem.jsx
+++ src/components/ToDoitem.jsx
@@ -1,11 +1,12 @@
 import React from "react";
+import {useState} from 'react';
 
 
 
 
 
 function ToDoItem(props){
-    // const[isDone,setIsDone]=useState(false);
+    const[isDone,setIsDone]=useState(false);
 
     // function dashChange(event){
     //  setIsDone((prevValue)=>{
@@ -19,9 +20,9 @@ function ToDoItem(props){
 
         props.onChecked(props.id)
     }}>
-        <li>{props.text}</li>
+        {/* <li>{props.text}</li> */}
         
-          {/* <li style={{textDecoration: isDone ? "line-through":"none"}}>{props.text}</li>) ; */}
+          <li style={{textDecoration: isDone ? "line-through":"none"}}>{props.text}</li>
     </div>
     
 
